e974a6e63c0fa722d7cc39a1e7239523a5051cc4
ccostoso <carlos.costoso@outlook.com>
2019-12-11 19:15:12 -0500

    Added more slide animations

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,29 +1,55 @@
 var windowNum = 0;
+var fluCount = 0;
+var coldCount = 0;
+var lastCounted;
+
+function proceed(action) {
+    var direction;
+    if (!action) {
+        direction = "left";
+    } else {
+        direction = "right";
+    }
+
+    $(`[data-qnum="${windowNum}"]`).removeClass("in-fade");
+    $(`[data-qnum="${windowNum}"]`).toggleClass(`${direction}-fade`);
+
+    setTimeout(function() {
+        $(`[data-qnum="${windowNum}"]`).toggleClass("d-none");
+        $(`[data-qnum="${windowNum}"]`).toggleClass(`${direction}-fade`);
+
+
+        if (!action) {
+                windowNum++;
+                $(".num-input").text(windowNum)
+                $(`[data-qnum="${windowNum}"]`).toggleClass("d-none in-fade");
+        } else {
+                windowNum--;
+                $(".num-input").text(windowNum)
+                $(`[data-qnum="${windowNum}"]`).toggleClass("d-none in-fade");
+                $(`[data-qnum="${windowNum}"]`).removeClass(`${direction}-fade`);
+        }
+    }, 400);
+}
 
 $("#start-button").on("click", function() {
-    $(`[data-qnum="${windowNum}"]`).toggleClass("left-fade");
-
-    setTimeout(function() {
-        $(`[data-qnum="${windowNum}"]`).toggleClass("d-none");
-        $(`[data-qnum="${windowNum}"]`).toggleClass("left-fade");
-
-        windowNum++;
-
-        $(`[data-qnum="${windowNum}"]`).toggleClass("d-none in-fade");
-        $(`[data-qnum="${windowNum}"]`).toggleClass("in-fade");
-    }, 400);
-})
+    proceed();
+});
 
 $(".back-button").on("click", function() {
-    $(`[data-qnum="${windowNum}"]`).toggleClass("right-fade");
+    proceed("back");
+});
 
-    setTimeout(function() {
-        $(`[data-qnum="${windowNum}"]`).toggleClass("d-none");
-        $(`[data-qnum="${windowNum}"]`).toggleClass("right-fade");
+$(".flu").on("click", function() {
+    proceed();
+    fluCount++;
+    lastCounted = "flu";
+    console.log("fluCount", fluCount);
+})
 
-        windowNum--;
-
-        $(`[data-qnum="${windowNum}"]`).toggleClass("d-none in-fade");
-        $(`[data-qnum="${windowNum}"]`).toggleClass("in-fade");
-    }, 400);
+$(".cold").on("click", function() {
+    proceed();
+    coldCount++;
+    lastCounted = "cold";
+    console.log("coldCount", coldCount);
 })
