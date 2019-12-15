f53037f8b3db5398afc01ea6ce6b48cfed32d355
Kimberly Suares <kimberlyjsuares@gmail.com>
2019-12-15 09:21:32 -0500

    moved first card symptoms

--- assets/js/results.js
+++ assets/js/results.js
@@ -15,7 +15,22 @@ $(document).ready(function () {
         console.log('data---->', typeof response.data );
     });
 
+/*------------------------------- First Card Symptoms------------------------------*/
 
+var helpNeeded="Find Doctor"; 
+var queryURL= "https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=4a76ed5c62af00d6fd94b0fa706cfbf6";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+    //  Trying to get the api of doctor location to be added into 
+    console.log('response----->', response);
+    // results[i]    .images.fixed_height.url
+    var rNose = $('#serious-symptom').attr("src", response.data[0].images.fixed_height.url);
+    console.log( typeof response.data );
+});
 
     /* ------------------------------Second Card ----------------------------------- */
     var preventativeCare = "remedies";
@@ -34,24 +49,5 @@ $(document).ready(function () {
     });
 
 });
-
-
-/*------------------------------- First Card Symptoms------------------------------*/
-
-var helpNeeded="Find Doctor"; 
-var queryURL= "https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=4a76ed5c62af00d6fd94b0fa706cfbf6";
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    //  Trying to get the api of doctor location to be added into 
-    console.log('response----->', response);
-    // results[i]    .images.fixed_height.url
-    var rNose = $('#serious-symptom').attr("src", response.data[0].images.fixed_height.url);
-    console.log( typeof response.data );
-});
-
 
 
