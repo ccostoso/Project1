d227d1d55e7fe678f7287f93209a1625b27d7e28
szeeepeiii <55262650+szeeepeiii@users.noreply.github.com>
2019-12-16 18:56:59 -0500

    1st push
    1st push

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,3 +1,5 @@
+// COLD
+
 $(document).ready(function () {
     console.log("ready!");
 
@@ -55,3 +57,90 @@ $.ajax({
 
 
 
+
+
+// FLU
+
+$(document).ready(function () {
+    console.log("ready!");
+
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 'fever' + "&api_key=UbAY6SJJhOljEzSrIOAedTGTZperCmLZ";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        //  Trying to get the image to be added into the img tag 
+        console.log('response----->', response);
+        // results[i]    .images.fixed_height.url
+        var rNose = $('#flu1').attr("src", response.data[3].images.fixed_height.url);
+        console.log('data---->', typeof response.data );
+    });
+
+
+
+    /* ------------------------------Second Card ----------------------------------- */
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "wash hands" + "&api_key=UbAY6SJJhOljEzSrIOAedTGTZperCmLZ";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        //  Trying to get the image to be added into the img tag 
+        console.log('response----->', response);
+        // results[i]    .images.fixed_height.url
+        var rNose = $('#flu2').attr("src", response.data[2].images.fixed_height.url);
+        console.log( typeof response.data );
+    });
+
+});
+
+// Youtube API
+var search= "tea";
+var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=tea&key=AIzaSyApou57R2okMqV6UNU5tGaGRB6qJ72YXFE";
+// response will be an array.  Each item in the array has an id.  so you will want to add that id to the end of the url below
+// "https://www.youtube.com/watch?v="+result[index].id       
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response)
+{ console.log("youtube", response);
+
+})
+
+
+  var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var playerInfoList = [{
+id: 'player1',
+videoId: 'qKYjj4VTksI'
+}, {
+id: 'player2',
+videoId: '4DI1fPyAssA'
+}, {
+id: 'player3',
+videoId: 'QvyaE_eXDJU'
+}];
+
+function onYouTubeIframeAPIReady() {
+if (typeof playerInfoList === 'undefined') return;
+
+for (var i = 0; i < playerInfoList.length; i++) {
+    var curplayer = createPlayer(playerInfoList[i]);
+    players[i] = curplayer;
+}
+}
+
+var players = new Array();
+
+function createPlayer(playerInfo) {
+return new YT.Player(playerInfo.id, {
+    videoId: playerInfo.videoId,
+});
+}
