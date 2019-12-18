800f26dedec2b85c463229f0b19c7dcf8b6ec790
Kimberly Suares <kimberlyjsuares@gmail.com>
2019-12-18 13:39:53 -0500

    rachel gif is working

--- assets/js/results.js
+++ assets/js/results.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         console.log('response----->', response);
         // results[i]    .images.fixed_height.url
         var rNose = $('#dripping-nose').attr("src", response.data[0].images.fixed_height.url);
-        console.log('data---->', typeof response.data );
+        console.log('data---->', typeof response.data);
     });
 
 
@@ -20,8 +20,8 @@ $(document).ready(function () {
 
     // need to create 1 on click event 
     // neeed to create a hover event 
-     $("#doctor-request").on("click", function(e){
-         e.preventDefault();
+    $("#doctor-request").on("click", function (e) {
+        e.preventDefault();
         var helpNeeded = "Find Doctor";
         var queryURL = "https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=4a76ed5c62af00d6fd94b0fa706cfbf6";
 
@@ -34,13 +34,13 @@ $(document).ready(function () {
             console.log('response----->', response);
             // results[i]    .images.fixed_height.url
             var rNose = $("#doctor-request").attr("src", response.data[0].profile.images_url);
-            var doctorArray= response.data;
-            for(var i=0; i<doctorArray.length; i++){
+            var doctorArray = response.data;
+            for (var i = 0; i < doctorArray.length; i++) {
                 $('#doctors-placeholder').append($('<p>').text(doctorArray[i].profile.bio));
                 $('#myModal').appendTo("body").modal('show');
                 console.log(doctorArray[i].profile.bio);
             }
-             console.log(typeof response.data);
+            console.log(typeof response.data);
         });
 
         /* ------------------------------Preventative Care Card ----------------------------------- */
@@ -60,144 +60,141 @@ $(document).ready(function () {
         });
 
 
-    /*------------------------------- First Card Symptoms------------------------------*/
+        /*------------------------------- First Card Symptoms------------------------------*/
 
 
-// need to create 1 on click event 
-// neeed to create a hover event 
+        // need to create 1 on click event 
+        // neeed to create a hover event 
 
-var helpNeeded="Find Doctor"; 
-var queryURL= "https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=4a76ed5c62af00d6fd94b0fa706cfbf6";
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    //  Trying to get the api of doctor location to be added into 
-    console.log('response----->', response);
-    // results[i]    .images.fixed_height.url
-    var rNose = $('#serious-symptom').attr("src", response.data[0].profile.images_url);
-    console.log( typeof response.data );
-});
+        var helpNeeded = "Find Doctor";
+        var queryURL = "https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=4a76ed5c62af00d6fd94b0fa706cfbf6";
 
 
-    /* ------------------------------Second Card ----------------------------------- */
-    var preventativeCare = "remedies";
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 'person working out' + "&api_key=UbAY6SJJhOljEzSrIOAedTGTZperCmLZ";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            //  Trying to get the api of doctor location to be added into 
+            console.log('response----->', response);
+            // results[i]    .images.fixed_height.url
+            var rNose = $('#serious-symptom').attr("src", response.data[0].profile.images_url);
+            console.log(typeof response.data);
+        });
 
+
+        /* ------------------------------Second Card ----------------------------------- */
+        var preventativeCare = "remedies";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 'person working out' + "&api_key=UbAY6SJJhOljEzSrIOAedTGTZperCmLZ";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            //  Trying to get the image to be added into the img tag 
+            console.log('response----->', response);
+            // results[i]    .images.fixed_height.url
+            var rNose = $('#working-out').attr("src", response.data[0].images.fixed_height.url);
+            console.log(typeof response.data);
+        });
+
+    });
+
+
+
+
+    $(document).ready(function () {
+        console.log("ready!");
+
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 'fever' + "&api_key=UbAY6SJJhOljEzSrIOAedTGTZperCmLZ";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            //  Trying to get the image to be added into the img tag 
+            console.log('response----->', response);
+            // results[i]    .images.fixed_height.url
+            var rNose = $('#flu1').attr("src", response.data[3].images.fixed_height.url);
+            console.log('data---->', typeof response.data);
+        });
+
+
+
+        /* ------------------------------Second Card ----------------------------------- */
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "wash hands" + "&api_key=UbAY6SJJhOljEzSrIOAedTGTZperCmLZ";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+
+        }).then(function (response) {
+            console.log(response);
+            //  Trying to get the image to be added into the img tag 
+            console.log('response----->', response);
+            // results[i]    .images.fixed_height.url
+            var rNose = $('#flu2').attr("src", response.data[1].images.fixed_height.url);
+            console.log(typeof response.data);
+        });
+
+    });
+
+    // Youtube API
+    var search = "tea";
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=tea&key=AIzaSyApou57R2okMqV6UNU5tGaGRB6qJ72YXFE";
+    // response will be an array.  Each item in the array has an id.  so you will want to add that id to the end of the url below
+    // "https://www.youtube.com/watch?v="+result[index].id       
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-        //  Trying to get the image to be added into the img tag 
-        console.log('response----->', response);
-        // results[i]    .images.fixed_height.url
-        var rNose = $('#working-out').attr("src", response.data[0].images.fixed_height.url);
-        console.log( typeof response.data );
-    });
+        console.log("youtube", response);
 
-});
+    })
 
 
+    var players = new Array();
 
 
-$(document).ready(function () {
-    console.log("ready!");
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 'fever' + "&api_key=UbAY6SJJhOljEzSrIOAedTGTZperCmLZ";
+    var playerInfoList = [{
+        id: 'player1',
+        videoId: 'qKYjj4VTksI'
+    }, {
+        id: 'player2',
+        videoId: '4DI1fPyAssA'
+    }, {
+        id: 'player3',
+        videoId: 'QvyaE_eXDJU'
+    }];
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        //  Trying to get the image to be added into the img tag 
-        console.log('response----->', response);
-        // results[i]    .images.fixed_height.url
-        var rNose = $('#flu1').attr("src", response.data[3].images.fixed_height.url);
-        console.log('data---->', typeof response.data );
-    });
+    function onYouTubeIframeAPIReady() {
+        if (typeof playerInfoList === 'undefined') return;
 
+        for (var i = 0; i < playerInfoList.length; i++) {
+            var curplayer = createPlayer(playerInfoList[i]);
+            players[i] = curplayer;
+        }
+    }
 
+    var players = new Array();
 
-    /* ------------------------------Second Card ----------------------------------- */
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "wash hands" + "&api_key=UbAY6SJJhOljEzSrIOAedTGTZperCmLZ";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-
-    }).then(function (response) {
-        console.log(response);
-        //  Trying to get the image to be added into the img tag 
-        console.log('response----->', response);
-        // results[i]    .images.fixed_height.url
-        var rNose = $('#flu2').attr("src", response.data[1].images.fixed_height.url);
-        console.log( typeof response.data );
-    });
-
-});
-
-// Youtube API
-var search= "tea";
-var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=tea&key=AIzaSyApou57R2okMqV6UNU5tGaGRB6qJ72YXFE";
-// response will be an array.  Each item in the array has an id.  so you will want to add that id to the end of the url below
-// "https://www.youtube.com/watch?v="+result[index].id       
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response)
-{ console.log("youtube", response);
-
-})
-
-
-        var players = new Array();
-
-
-  var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var playerInfoList = [{
-id: 'player1',
-videoId: 'qKYjj4VTksI'
-}, {
-id: 'player2',
-videoId: '4DI1fPyAssA'
-}, {
-id: 'player3',
-videoId: 'QvyaE_eXDJU'
-}];
-
-function onYouTubeIframeAPIReady() {
-if (typeof playerInfoList === 'undefined') return;
-
-for (var i = 0; i < playerInfoList.length; i++) {
-    var curplayer = createPlayer(playerInfoList[i]);
-    players[i] = curplayer;
-}
-}
-
-var players = new Array();
-
-function createPlayer(playerInfo) {
-    return new YT.Player(playerInfo.id, {
-        videoId: playerInfo.videoId,
-    });
-}
-
-<<<<<<< HEAD
     function createPlayer(playerInfo) {
         return new YT.Player(playerInfo.id, {
             videoId: playerInfo.videoId,
         });
     }
-// })
-=======
->>>>>>> 766e879d3cffafafba8712dd3ba0eb539ce1c7a8
+
+    function createPlayer(playerInfo) {
+        return new YT.Player(playerInfo.id, {
+            videoId: playerInfo.videoId,
+        });
+    }
+})
